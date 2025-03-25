b6cc8ba9ae66b170a2c8c7d01c22d5388779205c
BPKetterer <BenniKe2000@gmail.com>
2025-03-26 00:30:14 +0100

    made it (hopefully) compatible with already existing lls-love support

--- Love_LLS_support.lua
+++ Love_LLS_support.lua
@@ -1,16 +1,32 @@
-local love_api = require("Love_LLS_support.love-api.love_api")
-
----@alias detail_level # some text
----| "full" # prints all descriptions
----| "reduced" # prints only parameter names
----| "minimal" # prints only parameter types
+local love_api = require("love-api.love_api")
 
 local type_aliasas = {
     ["light userdata"]  = "lightuserdata",
     Variant = "love.Variant",
     tables = "table",
-    strings = "string"
+    strings = "string",
+    RenderTargetSetup = "Canvas"
 }
+
+---@type table[]
+local class_queue = {}
+---@type table[]
+local enum_queue = {}
+---@type {obj: table}
+local function_queue = {}
+local callback_queue = {}
+
+local fields = {} --todo remove
+
+---gets the alias of a type
+---@param type_name  string
+---@return string
+local function get_type_alias(type_name)
+    while type_aliasas[type_name] do
+        type_name = type_aliasas[type_name]
+    end
+    return type_name
+end
 
 local function human_table(t, indent)
     indent = indent or 0
@@ -34,13 +50,6 @@ local function human_table(t, indent)
     return s
 end
 
-local class_queue = {}
-local enum_queue = {}
-local function_queue = {}
-local table_args_queue = {}
-
-local fields = {} --todo remove
-
 local function split_str(str, delimiter)
     local result = {}
     for match in (str .. delimiter):gmatch("(.-)" .. delimiter) do
@@ -56,16 +65,27 @@ end
 ---formats description
 ---@param description string
 ---@return string
-local function format_description(description)
-    --description, _ = string.gsub(description, "\n\n", "\n")
-    --description, _ = string.gsub(description, "\n", "\n---")
+local function format_arg_desc(description, default)
+    description = description or ""
+    if default then
+        description = "(" .. default .. ") " .. description
+    end
     description , _ = string.gsub(description, "\n", "")
-    return description
+    
+    if description == "" then
+        return ""
+    else
+        return "# " .. description
+    end
 end
 
-local function temp_name(full_name)
-    local name , _ = string.gsub(full_name, "%.", "_")
-    return "temp__" .. name
+local function format_head_desc(description, wiki_name)
+    description = description or ""
+    description , _ = string.gsub(description, "\n", "\n---")
+    if wiki_name then
+        description = description .. "\n---\n---[Wiki](https://love2d.org/wiki/" .. wiki_name ..")\n---"
+    end
+    return "---" .. description
 end
 
 local function serialize_arg_type(arg)
@@ -90,7 +110,7 @@ local function serialize_arg_type(arg)
         return arg_type
     end
     if type_aliasas[arg_type] then
-        arg_type = type_aliasas[arg_type]
+        arg_type = get_type_alias(arg_type)
     end
     if arg.arraytype then
         arg_type = arg_type .. "[]"
@@ -139,27 +159,24 @@ local function serialize_arg_type(arg)
             arg_type = arg_type .. "[]"
         end
     end
-    if arg.default then
-        arg_type = arg_type .. "|nil"
-    end
     assert(arg.signature or arg.table or (not str_contains(arg_type, " ")), arg_type)
     return arg_type
 end
 
 ---prints a class
 ---@param f file*
----@param detail_level detail_level
 ---@param data table
-local function print_class(f, detail_level, data)
-    local full_name = data.full_name
-    local temp_name = data.temp_name
+local function print_class(f, data)
     local obj = data.obj
+    local full_name = data.full_name
+    local is_class = data.is_class
     if obj.version then
-        f:write("---@version " .. obj.version .. "\n")
+        f:write("---Version: " .. obj.version .. "\n")
     end
-    if obj.description and detail_level == "full" then
-        f:write("---" .. format_description(obj.description) .. "\n")
-    end
+    
+    local wiki_name = (is_class and obj.name) or full_name
+    f:write(format_head_desc(obj.description, wiki_name) .. "\n")
+
     f:write("---@class " .. full_name)
     local has_parent = false
     if obj.supertypes then
@@ -170,34 +187,37 @@ local function print_class(f, detail_level, data)
                 has_parent = true
                 f:write(" : ")
             end
-            f:write(type_aliasas[parent])
+            f:write(get_type_alias(parent))
         end
     end
     f:write("\n")
     if obj.modules then
         for _, v in pairs(obj.modules) do
-            f:write("---@field " .. v.name .. " " .. type_aliasas[v.name] .. "\n")
+            f:write("---@field " .. v.name .. " " .. get_type_alias(v.name) .. "\n")
         end
     end
-    f:write("local " .. temp_name .. " = {}\n")
+    if is_class then
+        f:write("local " .. obj.name .. " = {}\n")
+    else
+        f:write(full_name .. " = {}\n")
+    end
     f:write("\n")
 end
 
 ---prints an enum
 ---@param f file*
----@param detail_level detail_level
 ---@param data table
-local function print_enum(f, detail_level, data)
+local function print_enum(f, data)
     local full_name = data.full_name
     local obj = data.obj
-    f:write("---@alias " .. full_name )
-    if obj.description and detail_level == "full" then
-        f:write(" #" .. format_description(obj.description))
+    if obj.description then
+        f:write(format_head_desc(obj.description, obj.name) .. "\n")
     end
+    f:write("---@alias " .. full_name )
     f:write("\n")
     for _, con in pairs(obj.constants) do
-        if con.description and detail_level ~= "minimal" then
-            f:write("---|\"" .. con.name .. "\" # " .. format_description(con.description) .. "\n")
+        if con.description then
+            f:write("---|\"" .. con.name .. "\"\n" .. format_head_desc(con.description) .. "\n")
         else
             f:write("---|\"" .. con.name .. "\"\n")
         end
@@ -205,90 +225,101 @@ local function print_enum(f, detail_level, data)
     f:write("\n")
 end
 
-local function print_variant(f, detail_level, variant, full_name, temp_owner_name, name, header, takes_self)
+local function print_variant(f, variant, full_name, header)
     f:write(header)
-    local raw_args = ""
-    local has_arg = false
+    local raw_args = {}
     if variant.arguments then
         for _, arg in pairs(variant.arguments) do
             local arg_name, _ = string.gsub(arg.name, "'", "")
-            if has_arg then
-                raw_args = raw_args .. ", "
-            end
-            has_arg = true
-            raw_args = raw_args .. arg_name
-
-            local arg_type = serialize_arg_type(arg)
-
-            local arg_default = ""
-            if arg.default then
-                arg_default = " (" .. arg.default .. ")"
-            end
-
-            local arg_desciption = ""
-            if arg.description then
-                arg_desciption = " " .. format_description(arg.description)
-            end
-
-            local suffix = ""
-            if arg.default or arg.description then
-                suffix = "#" .. arg_default .. arg_desciption
-            end
+            table.insert(raw_args, arg_name)
 
             for _, arg_name in pairs(split_str(arg_name, ", ")) do
                 assert(not str_contains(arg_name, " "))
                 assert(not str_contains(arg_name, "\n"))
-                assert(not str_contains(suffix, "\n"))
-                f:write("---@param " .. arg_name .. " " .. arg_type .. suffix .. "\n")
+                if arg.default then
+                    arg_name = arg_name .. "?"
+                end
+                f:write("---@param " .. arg_name .. " " .. serialize_arg_type(arg) .. format_arg_desc(arg.description, arg.default) .. "\n")
             end
         end
     end
 
-    f:write("---@diagnostic disable-next-line: args-after-dots, duplicate-set-field\nfunction " .. temp_owner_name .. (takes_self and ":" or ".") .. name .. "(" .. raw_args .. ") end\n\n")
+    f:write("---@diagnostic disable-next-line: args-after-dots\nfunction " .. full_name .. "(" .. table.concat(raw_args, ", ") .. ") end\n\n")
 end
 
 ---prints a function
 ---@param f file*
----@param detail_level detail_level
 ---@param data table
-local function print_function(f, detail_level, data)
+local function print_function(f, data)
     local obj =  data.obj
-    local temp_owner_name = data.temp_owner_name
-    local takes_self = data.takes_self
-    local name = obj.name
     local full_name = data.full_name
     local header = ""
-    if obj.description and detail_level == "full" then
-        header = "---" .. format_description(obj.description) .. "\n"
+    if obj.description then
+        header = format_head_desc(obj.description, full_name) .. "\n"
     end
     
     for _, variant in pairs(obj.variants) do
-        print_variant(f, detail_level, variant, full_name, temp_owner_name, name, header, takes_self)
+        print_variant(f, variant, full_name, header)
     end
+end
+
+local function print_callback(f, data)
+    local obj =  data.obj
+    local full_name = data.full_name
+    local variant_strs = {}
+    --f:write(human_table(obj), "\n")
+    for _, variant in pairs(obj.variants) do
+        local arg_strs = {}
+        local retruen_strs = {}
+        if variant.arguments then
+            for _, arg in pairs(variant.arguments) do
+                table.insert(arg_strs, arg.name .. ": " .. serialize_arg_type(arg))    
+            end
+        end
+        if variant.returns then
+            for _, re in pairs(variant.returns) do
+                if re.type == "function" then
+                    table.insert(retruen_strs, "fun()")
+                else
+                    table.insert(retruen_strs, serialize_arg_type(re))
+                end 
+            end
+        end
+        local variant_str = "fun(" .. table.concat(arg_strs, ", ") .. ")"
+        if variant.returns then
+            variant_str = variant_str .. ":" .. table.concat(retruen_strs, ", ")
+        end
+        table.insert(variant_strs, variant_str)
+    end
+    f:write(format_head_desc(obj.description, full_name) .. "\n")
+    f:write("---@alias " .. full_name .. " ")
+    f:write(table.concat(variant_strs, "|"), "\n\n")
 end
 
 ---prints all element from a queue
 ---@param queue table
----@param fun fun(file*, table, string, detail_level)
+---@param fun fun(file*, table, string, )
 ---@param f file*
----@param detail_level detail_level
-local function print_queue(queue, fun, f, detail_level)
+local function print_queue(queue, fun, f)
     for _, item in pairs(queue) do
-        fun(f, detail_level, item)
+        fun(f, item)
     end
 end
 
 ---prints the docu
 ---@param f file*
----@param detail_level detail_level
-local function print_all(f, detail_level)
-    f:write("---@alias love.Variant table|boolean|string|number|" .. type_aliasas["Object"] .. "\n")
+local function print_all(f)
+    f:write("---@meta\n")
+    f:write("--Autogenerated\n")
     f:write("\n")
-    print_queue(class_queue, print_class, f, detail_level)
-    print_queue(enum_queue, print_enum, f, detail_level)
-    print_queue(function_queue, print_function, f, detail_level)
-    f:write("---@type loveModule.love\n")
-    f:write("love = love\n")
+    f:write("assert(false, \"This file is only for LuaLS and should not be included\")\n")
+    f:write("\n")
+    f:write("---@alias love.Variant table|boolean|string|number|" .. get_type_alias("Object") .. "\n")
+    f:write("\n")
+    print_queue(class_queue, print_class, f)
+    print_queue(callback_queue, print_callback, f)
+    print_queue(enum_queue, print_enum, f)
+    print_queue(function_queue, print_function, f)
 end
 
 ---collects the data of the module/type
@@ -296,48 +327,50 @@ end
 ---@param path string|nil
 ---@param is_class boolean|nil
 local function collect(obj, path, is_class)
-    if path then
+    assert(obj)
+    local full_name = "love"
+    if is_class then
         assert(obj.name)
-        path = path .. "." .. obj.name
-    else
-        path = "love"
+        full_name = "love." .. obj.name    
+    elseif path then
+        assert(obj.name)
+        full_name = path .. "." .. obj.name
     end
-    --gets the full name of the module/type
-    local full_name = path
-    if not is_class then
-        full_name = "loveModule." .. full_name
-    end
-    --generates the temp name
-    local temp_name = temp_name(full_name)
     --adds the module to the alias lookup
     if obj.name then
         type_aliasas[obj.name] = full_name
     end
     --adds the module/type to the print queue
-    table.insert(class_queue, {full_name = full_name, temp_name = temp_name, obj = obj})
+    table.insert(class_queue, {obj = obj, full_name = full_name, is_class = is_class})
     --search for children
     for k, v in pairs(obj) do
         if k == "modules" then
             --recursivly collects modules
             for _, module in pairs(v) do
-                collect(module, path, false)
+                collect(module, full_name, false)
             end
         elseif k == "types" then
             --recursivly collects types
             for _, type in pairs(v) do
-                collect(type, path, true)
+                collect(type, full_name, true)
             end
         elseif k == "enums" then
-            ---@todo
             for _, enum in pairs(v) do
-                local enum_full_name = full_name .. "." .. enum.name
-                type_aliasas[enum.name] = enum_full_name
-                table.insert(enum_queue, {full_name = enum_full_name, obj = enum})
+                local full_enum_name = "love." .. enum.name
+                type_aliasas[enum.name] = full_enum_name
+                table.insert(enum_queue, {obj = enum, full_name = full_enum_name})
             end
-        elseif k == "functions" or k == "callbacks" then
+        elseif k == "callbacks" then
+            for _, callback in pairs(v) do
+                table.insert(callback_queue, {obj = callback, full_name = full_name .. "." .. callback.name})
+            end
+        elseif k == "functions"then
             --collects functions/callbacks
+            local fun_name_prefix = ((
+                    is_class and (obj.name .. ":"))
+                    or full_name .. ".")
             for _, fun in pairs(v) do
-                table.insert(function_queue, {temp_owner_name = temp_name, obj = fun, full_name = path .. fun.name, takes_self = is_class})
+                table.insert(function_queue, {obj = fun, full_name = fun_name_prefix .. fun.name})
             end
         elseif k == "name" or k == "version" or k == "supertypes" or k == "description" or k == "constructors" then
         else
@@ -348,20 +381,22 @@ end
 
 ---creates a lls documentation
 ---@param file_path string: the destination
----@param detail_level detail_level
-return function(file_path, detail_level)
+return function(file_path)
     local file = io.open(file_path, "w")
     assert(file)
-    file:write("--Autogenerated\n\n")
+    file:write("")
     file:close()
     file = io.open(file_path, "a")
     assert(file)
 
     collect(love_api)
+
+    ---refer RenderTargetSetup type
     if not type_aliasas["RenderTargetSetup"] then
-        type_aliasas["RenderTargetSetup"] = type_aliasas["Canvas"]
+        type_aliasas["RenderTargetSetup"] = get_type_alias("Canvas")
     end
-    print_all(file, detail_level)
+    
+    print_all(file)
 
     for k, _ in pairs(fields) do
         file:write(k .. "\n")
